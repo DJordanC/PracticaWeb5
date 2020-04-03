7a7fadf9bd12a335c8e4875b8ab3d6a9ac12d86c
DJordanC <pnfiduneska@gmail.com>
2020-04-03 13:39:34 -0400

    Initial Commit

---
+++ Inicio.php
@@ -0,0 +1,81 @@
+
+<?php
+// primer video
+	echo ("Hola Mundo como va la cuarentena");
+?>
+
+
+/*<?php
+/*
+// Variables y Concatenar
+	$edad = 20;
+	$nombre = 'Pedro'
+
+		echo $edad;
+		echo $nombre. 'tiene: ' .$edad 'años';
+*/
+?>
+
+// Condicionales
+<?php
+/*	
+// Condicionales
+	$dinero = 4999;
+
+		if($dinero>2999){
+			echo 'Puede ir a comprar';
+	    }else{
+			echo'No puedes ir a comprar';
+}*/
+?>
+
+<?php
+// Operdores
+	$num1 = 5;
+	$num2 = 4;
+	$num3 = 8
+
+/*	$suma = $num1 + $num2;
+		echo $suma;
+	$resta = $num1 - $num2;
+		echo $resta;
+	$multiplicar = $num1 * $num2;
+		echo $multiplicar
+*/
+
+/*
+	if($num1>num2){
+		echo 'es verdad';
+	}else{
+		echo 'es falso';
+	}
+	
+    if($num1>num2 || $num3<$num2){
+		echo 'es verdad';
+	}else{
+		echo 'es falso';
+	}
+
+	$num1 = $num+1;
+	$num1++;
+		echo $num1;
+
+
+*/
+
+?>
+
+<?php
+/*	
+// While Do While
+	$i = 2;
+		while($i<10){
+			echo $1;
+			$i++;
+	}do{
+		echo $i.'<br>';
+	}
+	while($i<10);
+		echo'Fin del Ciclo do';
+*/
+?>
